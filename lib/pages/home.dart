a1c9b391fbed0ede529140e611d19f819f75c242
Murad EIssa <muradissa00@gmail.com>
2023-10-08 00:08:23 +0300

    add drawer

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,41 +9,79 @@ class Home extends StatelessWidget{
     Widget build(BuildContext context){
       return SafeArea(child:
           Scaffold(
-          appBar: AppBar(
-            title: Text("Home"),
-            backgroundColor: appBarBlue,
-            actions :[
-               Row(
+            drawer: Drawer(
+
+              child: Column(
                 children: [
-                  Stack(
-                    children: [
-                      IconButton(
-                          onPressed: (){},
-                          icon: Icon(Icons.add_shopping_cart)
+                  UserAccountsDrawerHeader(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(image: AssetImage("assets/img/back.jpg") ,fit: BoxFit.cover),
                       ),
-                      Container(
-                        child:Text("20" ,style: TextStyle(color: Colors.black,),),
-                        padding: EdgeInsets.all(3),
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 40, 180, 241 ),
-                          shape: BoxShape.circle,
-                        ),
+                      accountName: Text("Murad Issa"),
+                      accountEmail: Text("examle@email.com"),
+                      currentAccountPicture: CircleAvatar(
+                        radius: 50,
+                        backgroundImage: AssetImage("assets/img/avatar.jpg"),
                       ),
-                    ],
+                      //currentAccountPictureSize: Size.square(99),
                   ),
+                  ListTile(title: Text("Home"),leading: Icon(Icons.home),onTap: (){} ,),
+                  ListTile(title: Text("My products"),leading: Icon(Icons.add_shopping_cart),onTap: (){} ,),
+                  ListTile(title: Text("About"),leading: Icon(Icons.help_center),onTap: (){} ,),
+                  Expanded(flex:2,child: Text("")),
+                  ListTile(title: Text("Logout"),leading: Icon(Icons.exit_to_app),onTap: (){} ,),
                   Padding(
-                    padding: const EdgeInsets.only(right: 15.0),
+                    padding: const EdgeInsets.only(bottom:  5.0),
                     child:
-                      Text(
-                          "\$ 202",
-                          style: TextStyle(fontSize: 18)
-                      ),
+                    Text(
+                        "Developed by Murad Issa 2023",
+                        style: TextStyle(fontSize: 13)
+                    ),
                   )
+
+
+
                 ],
-              )
-           ]
-          ),
-        )
+              ),
+
+            ),
+
+
+            appBar: AppBar(
+              title: Text("Home"),
+              backgroundColor: appBarBlue,
+              actions :[
+                 Row(
+                  children: [
+                    Stack(
+                      children: [
+                        IconButton(
+                            onPressed: (){},
+                            icon: Icon(Icons.add_shopping_cart)
+                        ),
+                        Container(
+                          child:Text("20" ,style: TextStyle(color: Colors.black,),),
+                          padding: EdgeInsets.all(3),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 40, 180, 241 ),
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 15.0),
+                      child:
+                        Text(
+                            "\$ 202",
+                            style: TextStyle(fontSize: 18)
+                        ),
+                    )
+                  ],
+                )
+             ]
+            ),
+          )
       );
     }
 
