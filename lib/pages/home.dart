04bc8baa5f5bb0bb79e85c18bf622baccb82d770
Murad EIssa <muradissa00@gmail.com>
2023-10-09 14:03:28 +0300

    add home page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,23 @@
 import 'package:flower_store_app/shared/colors.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
+final List<Map> products = List.generate(8, (index) => {
+      "id": index,
+      "name": "Flower $index",
+      "price": (Random().nextInt(80)+20)
+    }).toList();
 
 class Home extends StatelessWidget{
-    const Home({Key?key}) : super(key: key);
+
+  const Home({Key?key}) : super(key: key);
+
 
     @override
     Widget build(BuildContext context){
       return SafeArea(child:
           Scaffold(
             drawer: Drawer(
-
               child: Column(
                 children: [
                   UserAccountsDrawerHeader(
@@ -38,14 +45,9 @@ class Home extends StatelessWidget{
                         style: TextStyle(fontSize: 13)
                     ),
                   )
-
-
-
                 ],
               ),
-
             ),
-
 
             appBar: AppBar(
               title: Text("Home"),
@@ -80,6 +82,100 @@ class Home extends StatelessWidget{
                   ],
                 )
              ]
+            ),
+
+            body: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 2/3,
+                      crossAxisSpacing: 10 ,
+                      mainAxisSpacing: 33,
+
+                  ),
+                  itemCount: products.length,
+                  itemBuilder: (BuildContext context ,int index){
+                    // return Container(
+                    //   color: Color.fromARGB(10, 2, 2, 2),
+                    //
+                    //   child:  Column(
+                    //
+                    //     children: [
+                    //       GridTile(
+                    //           child:
+                    //               ClipRRect(
+                    //                 borderRadius: BorderRadius.circular(500),
+                    //                 child :Image(
+                    //                   image: AssetImage("assets/img/flower"+index.toString()+".png") ,
+                    //                   fit: BoxFit.cover,
+                    //                   height:180,
+                    //                 ),
+                    //               )
+                    //       ),
+                    //       Text(index.toString())
+                    //     ],
+                    //   ),
+                    // );
+
+                    // return GridTile(
+                    //
+                    //     child: Stack(
+                    //       children: [
+                    //         Positioned(
+                    //           right: 10,
+                    //             left: 10,
+                    //             bottom: 20,
+                    //             top: 10,
+                    //             child: ClipRRect(
+                    //               borderRadius: BorderRadius.circular(30),
+                    //               child: Image.asset("assets/img/flower"+index.toString()+".png"),
+                    //
+                    //           )
+                    //         ),
+                    //       ],
+                    //     ),
+                    //   footer: Text(index.toString(),style: TextStyle(),textAlign: TextAlign.center)
+                    //   ,
+                    // );
+                    return Container(
+                      color: Color.fromARGB(20, 2, 2,2),
+                      //
+                      //padding: EdgeInsets.only(top: 20,bottom: 20),
+                      child: GestureDetector(
+                        child: GridTile(
+                          //key: ValueKey(_products[index]['id']),
+                          footer: GridTileBar(
+                            backgroundColor: Colors.black54,
+                            title: Text(
+                              products[index]['name'],
+                              style: const TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text("\$${products[index]['price'].toString()}"),
+                            trailing: const Icon(Icons.shopping_cart),
+                          ),
+
+                              child: Stack(
+                                children: [
+                                  Positioned(
+                                    right: 10,
+                                      left: 10,
+                                      bottom: 30,
+                                      top: 10,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(30),
+                                        child: Image.asset("assets/img/flower"+index.toString()+".png"),
+
+                                    )
+                                  ),
+                                ],)
+                        ),
+                        onTap: (){},
+                      ),
+                    );
+                  }
+              ),
             ),
           )
       );
